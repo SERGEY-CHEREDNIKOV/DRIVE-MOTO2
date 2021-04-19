7390eebae4c06005ad531d130eca56e6e75f889a
Sergey Cherednikov <serezha.ros@mail.ru>
2021-04-19 07:22:10 +0300

    first commit

---
+++ source/js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function () {
+
+    "use strict";
+
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button class="banner-section__slider-btn  banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt="стрелка влево"></button>',
+        nextArrow: '<button class="banner-section__slider-btn  banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt="стрелка вправо"></button>',
+    })
+
+});
